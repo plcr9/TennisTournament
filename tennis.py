6dfc01d6bbba82c6a928b5022fa41f3415797d59
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-22 17:55:47 +0100

    Update tennis.py

--- tennis.py
+++ tennis.py
@@ -15,6 +15,24 @@ class Player:
       f"ranking_points={self.ranking_points})"
     )
 
+class Unit:
+  def __init__(self, players=(Player(), Player())):
+    self.players = players
+    self.score = {
+      self.players[0]: 0,
+      self.players[1]: 0,
+    }
+    self.winner = None
+
+  def get_winner(self):
+    return self.winner
+
+  def get_score(self):
+    return self.score
+
+  def is_running(self):
+    return self.winner == None
+
 class Match:
   def __init__(
     self,
