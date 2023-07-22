6f185d0e3c376b89cc0d065e6046c3213c9dadee
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-22 16:23:49 +0100

    Update tennis.py

--- tennis.py
+++ tennis.py
@@ -1,1 +1,16 @@
+class Player:
+  def __init__(self, name-"", ranking_points=0):
+    self.name = name
+    self.ranking_points = ranking_points
 
+class Match:
+  def __init__(
+    self,
+    player_1=Player(),
+    player_2=Player(),
+    best_of_5=True,
+  ):
+    self.players = (player_1, player_2)
+    self.best_of_5 = best_of_5
+    self.sets_to_play = 5 if best_of_5 else 3
+  
